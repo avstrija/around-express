48c9096ff04fc31cdb4a24e2d86c30cac767c1c6
Dasha Borisenko-Orlowski <weissebrauen@yandex.ru>
2020-12-30 10:46:10 -0500

    Validate alphanumeric password

--- models/user.js
+++ models/user.js
@@ -37,6 +37,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     select: false,
+    validate: {
+      validator: (v) => validation.isAlphanumeric(v),
+      message: 'Password should not contain spaces',
+    },
   },
 });
 
