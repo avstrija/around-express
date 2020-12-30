eaf2ebf311cfa9594aaf56c1df876b89ed1ee525
Dasha Borisenko-Orlowski <weissebrauen@yandex.ru>
2020-12-30 12:40:23 -0500

    create logs dir

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,13 @@
 const winston = require('winston');
+const fs = require('fs');
 const path = require('path');
 const expressWinston = require('express-winston');
 
 const logDir = 'logs';
+if (!fs.existsSync(logDir)) {
+  // Create the directory if it does not exist
+  fs.mkdirSync(logDir);
+}
 
 const requestLogger = expressWinston.logger({
   transports: [
